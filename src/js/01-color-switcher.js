2fdf9623846b36e2fbce5c59a4ce6317088ac0e2
Aleksandr Harianin <onlydipro@gmail.com>
2023-07-17 23:53:57 +0300

    complete first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBTN = document.querySelector('[data-start]');
+const stopBTN = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let timeId = null;
 
+startBTN.addEventListener('click', startColorChange);
+stopBTN.addEventListener('click', stopColorChange);
+
+function startColorChange() {
+  if (timeId === null) {
+    startBTN.disabled = true;
+    timeId = setInterval(changeBackgroundColor, 1000);
+  }
+}
+
+function stopColorChange() {
+  if (timeId !== null) {
+    startBTN.disabled = false;
+    clearInterval(timeId);
+    timeId = null;
+  }
+}
+
+function changeBackgroundColor() {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
